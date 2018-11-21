0f7777770bd46d9011ae11e2aa9a52d11e7c5918
libinmath3w <musichevenindia@gmail.com>
2018-11-21 23:11:23 +0530

    map activity added

--- app/src/main/java/com/aksharam/greenpooling/activity/OfferRide.java
+++ app/src/main/java/com/aksharam/greenpooling/activity/OfferRide.java
@@ -1,27 +1,71 @@
 package com.aksharam.greenpooling.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.aksharam.greenpooling.R;
+import com.aksharam.greenpooling.helper.SQLiteHandler;
+import com.aksharam.greenpooling.helper.SessionManager;
+
+import java.util.HashMap;
 
 public class OfferRide extends AppCompatActivity {
     private Button ok,cancel;
-    private Spinner avlseats;
-    private String frommapstemp , localfrom;
+    private Spinner avlseats,my_vehicle;
+    private String frommapstemp , localfrom,vehiclesspinner;
     private EditText from,dest,times;
     private ImageView fromgps,destgps;
+    private SQLiteHandler db;
+    private ProgressDialog pDialog;
+    private SessionManager session;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.offer_ride);
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // Progress dialog
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        // Fetching user details from SQLite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        final String email = user.get("email");
+
+        final Spinner dropdown = findViewById(R.id.my_vehicles);
+        String[] items = new String[]{"CAR", "BIKE"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
+        vehiclesspinner = dropdown.getSelectedItem().toString();
+        final Spinner seats = findViewById(R.id.available_seats);
+        if (vehiclesspinner.equals("CAR")) {
+            String[] item = new String[]{"1", "2","3", "4","5", "6"};
+            ArrayAdapter<String> adapters = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, item);
+            seats.setAdapter(adapters);
+        }
+        else {
+            String[] item = new String[]{"1"};
+            ArrayAdapter<String> adapters = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, item);
+            dropdown.setAdapter(adapters);
+        }
         fromgps = (ImageView)findViewById(R.id.fromgps);
         destgps = (ImageView)findViewById(R.id.destgps);
         from = (EditText)findViewById(R.id.et_from);
@@ -55,6 +99,14 @@ public class OfferRide extends AppCompatActivity {
         });
         String destmap = getIntent().getStringExtra("dest");
             dest.setText(destmap);
+
+            ok.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Toast.makeText(getApplicationContext(),vehiclesspinner,Toast.LENGTH_LONG).show();
+                }
+            });
 
 
 
